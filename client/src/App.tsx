76ad153f2f89491a0edd5471244e958e67bfac47
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-05-24 11:03:44 +0800

    Connected frontend to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+export type apiType = {
+  users: string[]
+}
 
 function App() {
+
+  const [backendData, setBackendData] = useState<apiType | null>()
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof backendData?.users === 'undefined') ? (
+        <p>Loading ...</p>
+      ) : (
+        backendData?.users.map((user,index) => (
+          <p key={index}>{user}</p>
+        ))
+      )}
     </div>
   );
 }
