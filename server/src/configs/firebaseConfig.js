47c82c8c9ab7958d3642b6a35987bbe663e95290
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-07-12 22:15:04 +0800

    update: reformat firebase service account key

--- server/src/configs/firebaseConfig.js
+++ server/src/configs/firebaseConfig.js
@@ -2,8 +2,10 @@ const admin = require("firebase-admin")
 const serviceAccount = require("./serviceAccountKey.json")
 require("dotenv").config()
 
+const serviceAccountcert = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountcert),
     storageBucket: process.env.STORAGE_BUCKET
 })
 
