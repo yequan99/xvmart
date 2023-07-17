33bcec2ceeb4ed4ba94f7fd11a07c42f5217e766
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-07-17 21:55:10 +0800

    update firebase config

--- server/src/configs/firebaseConfig.js
+++ server/src/configs/firebaseConfig.js
@@ -1,7 +1,9 @@
 const admin = require("firebase-admin")
+const { stringify } = require("ordered-json")
 require("dotenv").config()
 
-const serviceAccountcert = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+const cert = stringify(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+const serviceAccountcert = JSON.parse(cert)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountcert),
