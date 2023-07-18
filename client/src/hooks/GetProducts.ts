1638fcb094f694393b77b70e5292923369ad0063
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-07-18 21:56:41 +0800

    update

--- client/src/hooks/GetProducts.ts
+++ client/src/hooks/GetProducts.ts
@@ -2,7 +2,12 @@ import { Dispatch, SetStateAction } from 'react'
 import { ApiProps } from '../types/mainTypes'
 
 export default async function GetProducts(setBackendData: Dispatch<SetStateAction<ApiProps | null>>) {
-    const response = await fetch("/product")
+    const response = await fetch("/product", {
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
     const data = await response.json()
     setBackendData(data)
 }
