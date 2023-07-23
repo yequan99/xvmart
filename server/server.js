1e8e702083e2b48660d67af045bf96e7da8cfce5
ye_quan_tan <ytan213@e.ntu.edu.sg>
2023-07-23 16:57:47 +0800

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
 require("dotenv").config()
 const PORT = process.env.PORT || 5000
 
